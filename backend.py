1fcda225b88260ad6c255ef724b7580ae73c1c49
quinncerino <quinncerino@gmail.com>
2025-07-19 16:26:20 -0400

    made get request using api url

--- backend.py
+++ backend.py
@@ -2,3 +2,8 @@ import requests
 import os
 
 API_KEY = os.getenv("FORECASTING")
+
+url = f"http://api.openweathermap.org/data/2.5/forecast?q=Paris&appid={API_KEY}"
+response = requests.get(url)
+data = response.json()
+print(data)
